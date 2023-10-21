15b614bccc557069e85534b0363bd892592a701c
sthereis245 <145220962+sthereis245@users.noreply.github.com>
2023-10-21 17:29:26 -0300

    21/10/23

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// tres constantes para representar as ITEM nas Divs Projetos//
+const form= document.getElementById('album de Fotos');
+const site= document.getElementById('Casamento');
+const port = document.getElementById('Buffet');
+
+const videoAlbum=document.querySelector('.video album');
+const videoCasamento=document.querySelector('.videoCasamento');
+const videoBuffet=document.querySelector('.videoBuffet');
+//evento de click
+album.form.addEventListener('click',()=>{
+ album.form.addEventListener.toggle('active');
+videoAlbum.classList.toggle('acive');
+
+
+
+});
